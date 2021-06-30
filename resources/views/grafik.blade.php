085edd849ae9e2512ca0103de3078f1dc3c5a360
AryaNandaMP <67847087+AryaNandaMP@users.noreply.github.com>
2021-06-30 11:19:43 +0800

    Create grafik.blade.php

---
+++ resources/views/grafik.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.template')
+@section('title','Home')
+@section('content')
+@endsection
+
+<script src="{{ asset('assets/plugins/chart.js/Chart.min.js') }}"></script>
+<script src="{{ asset('assets/dist/js/demo.js') }}"></script>
+<script src="{{ asset('assets/dist/js/pages/dashboard3.js') }}"></script>
+
+@endpush
